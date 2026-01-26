f3a3e05bdf549b8436f51b0f0edd2bd619601cc4
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-01-26 16:35:17 +0200

    ccc

--- ViritrapiStackEbiv.js
+++ ViritrapiStackEbiv.js
@@ -347,7 +347,7 @@ const VibeRouteNebula = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-01-22T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-01-29T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
